41f1b1e9c19a612816a9246aa45f8a426168eb96
Giovanny <giovannycostaamaral6@gmail.com>
2024-03-17 16:27:56 -0300

    Add cursoJS folder with lessons

---
+++ cursoJS/aula 2/index.js
@@ -0,0 +1,42 @@
+console.log('Giovanny Costa');  //exemplo;   console.log('Giovanny "Costa"')
+console.log("Giovanny Costa");  //exemplo;   console.log("Giovanny 'Costa'")
+console.log(`Giovanny Costa`);  //exemplo;   console.log(`Giovanny '"Costa"'`)
+
+//isso quer dizer que aspas simples ou aspas duplas ou crase, vai apenas
+//adicionar "" ou '' ou `` aonded eu usar aspas a mais.
+//por exemplo: console.log('Giovanny "Costa"')
+//se eu quiser mostrar isso no terminal ou no console, vai aparecer exatamente:
+//Giovanny "Costa"
+
+console.log(9867767)
+
+//console.log é usado geralmente para textos.
+//porem, quando eu der por exemplo
+//console.log(9237856) o motor do js vai entender normalmente.
+//nao iria ser preciso algum outro tipo de comando.
+
+console.log(123.423)
+
+//ao invez de usar virgulas, usamos pontos
+//para separa casas decimas no js.
+//nao existe limite para o console.log
+//eu posso colocar quantas caracteries eu quiser.
+
+console.log(34.43, 'Giovanny Costa')
+
+//console.log nao quebra alinha.
+//ent por exemplo se eu quiser colocar 
+//console.log(24. 43, powershell) e colocar no console, vai sair assim: 
+//24. 43, powershell
+//porem nao posso esquecer do '' ou "" ou `` 
+
+console.log("galaxia")
+
+//tudo que esteja entre "" se enquadra como string = texto.
+
+console.log(3545654)
+
+//todos os numeros do console.log que nao apresentarem "", '', ``
+//sao classificados como number = numero.
+
+
---
+++ cursoJS/aula 5/script.js
@@ -0,0 +1,27 @@
+function meuEscopo () {
+    const form = document. querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = {};
+
+    function recebeEventoForm (evento ) {
+    evento.preventDefault();
+
+    const nome = form.querySelector('.nome')
+    const sobrenome = form.querySelector('.sobrenome')
+    const peso = form.querySelector('peso')
+    const altura = form.querySelector('.altura')
+
+    pessoas.push({
+      nome: nome.Value
+      sobrenome: sobrenome.Value
+      peso: peso.Value
+      altura: altura.value
+    });
+    
+console.log(pessoas);
+
+resultado.innerHTML += '<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>'
+}
+}
+meuEscopo(); 
